6304ea0561bb370e01efb3ba7ba24086dd4c1b8d
AsgerMorville <48725961+AsgerMorville@users.noreply.github.com>
2019-06-14 20:46:16 +0200

    Add files via upload

---
+++ load/functions.R
@@ -0,0 +1,5 @@
+#Source function
+source("load/loadPack.R")
+source("convert/conv2magick.R")
+source("Simulation/smallSim3d.R")
+source("simulation test/addnoisetopicture.R")
---
+++ load/loadPack.R
@@ -0,0 +1,8 @@
+#Load packages
+library(spatstat)
+library(MASS)
+library(misc3d)
+library(ggplot2)
+library(tiff)
+library(magick)
+#library(spatialfil)
